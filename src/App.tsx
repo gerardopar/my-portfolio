5ddb2c7106a7d0dade8887f56fe66024ebe085f5
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-09 13:20:36 -0700

    setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 export const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-[100vh] w-[100vw] bg-black">
+      <div className="w-full h-full flex items-center justify-center">
+        <h1 className="text-3xl text-white font-inter font-[600] tracking-wide">
+          Developer
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default App;
