ebc3234f97053845ff0f6dab380e8884ab7b6408
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-09 21:50:05 -0700

    add glitch effect

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,17 +1,28 @@
 import React from "react";
-
 import { Link } from "react-router";
 
 export const Nav: React.FC = () => {
   return (
     <nav className="flex gap-12">
-      <Link className="text-primary-white font-inter font-[200]" to="/about">
+      <Link
+        className="glitch text-primary-white font-inter font-[200]"
+        to="/about"
+        data-text="About"
+      >
         About
       </Link>
-      <Link className="text-primary-white font-inter font-[200]" to="/projects">
+      <Link
+        className="glitch text-primary-white font-inter font-[200]"
+        to="/projects"
+        data-text="Projects"
+      >
         Projects
       </Link>
-      <Link className="text-primary-white font-inter font-[200]" to="/contact">
+      <Link
+        className="glitch text-primary-white font-inter font-[200]"
+        to="/contact"
+        data-text="Contact"
+      >
         Contact
       </Link>
     </nav>
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 import "./index.css";
+import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
