74e399b06dc617f0f3e3ef3dece6b84bf36e4443
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-12 15:46:19 -0700

    add backdrop blur to topbar

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -5,7 +5,8 @@ import Divider from "../shared/Divider";
 
 export const Topbar: React.FC = () => {
   return (
-    <header className="fixed left-0 top-0 h-[50px] w-[100vw] bg-primary-black px-24 pt-8 pb-4">
+    <header className="fixed left-0 top-0 h-[80px] w-[100vw] px-24 py-6 z-50">
+      <div className="absolute top-0 left-0 w-[100vw] h-[30px] backdrop-blur-[2px]" />
       <div className="flex items-center justify-between gap-8">
         <p className="text-primary-white font-jetbrains font-bold">
           Gerardo Paredes
