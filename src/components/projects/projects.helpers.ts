deb41631b3cf64242ca7265981c8f4c92fd13e79
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-12 15:46:48 -0700

    add projecy helpers

---
+++ src/components/projects/projects.helpers.ts
@@ -0,0 +1,67 @@
+import LumeThumbnail from "@assets/images/lume-thumb.png";
+
+interface Project {
+  title: string;
+  description: string;
+  techStack: string[];
+  image: string;
+  caseStudyUrl?: string;
+  liveDemoUrl?: string;
+  sourceCodeUrl?: string;
+}
+
+const projects: Project[] = [
+  {
+    title: "Lume – Movie Management Platform",
+    description:
+      "Lume is a smart movie platform that helps you discover what to watch next with AI-driven recommendations, personal watchlists, and favorites — all wrapped in a clean, minimalist interface inspired by modern streaming apps.",
+    techStack: [
+      "React",
+      "TypeScript",
+      "tRPC",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "TMDB API",
+    ],
+    image: LumeThumbnail,
+    liveDemoUrl: "https://lume-client.onrender.com/",
+    sourceCodeUrl: "https://github.com/gerardopar/lume",
+  },
+  {
+    title: "Lume – Movie Management Platform",
+    description:
+      "Lume is a smart movie platform that helps you discover what to watch next with AI-driven recommendations, personal watchlists, and favorites — all wrapped in a clean, minimalist interface inspired by modern streaming apps.",
+    techStack: [
+      "React",
+      "TypeScript",
+      "tRPC",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "TMDB API",
+    ],
+    image: LumeThumbnail,
+    liveDemoUrl: "https://lume-client.onrender.com/",
+    sourceCodeUrl: "https://github.com/gerardopar/lume",
+  },
+  {
+    title: "Lume – Movie Management Platform",
+    description:
+      "Lume is a smart movie platform that helps you discover what to watch next with AI-driven recommendations, personal watchlists, and favorites — all wrapped in a clean, minimalist interface inspired by modern streaming apps.",
+    techStack: [
+      "React",
+      "TypeScript",
+      "tRPC",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "TMDB API",
+    ],
+    image: LumeThumbnail,
+    liveDemoUrl: "https://lume-client.onrender.com/",
+    sourceCodeUrl: "https://github.com/gerardopar/lume",
+  },
+];
+
+export default projects;
