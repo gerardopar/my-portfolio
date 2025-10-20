bb60c3945848f4830234f1c728c02588f2bac79d
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-19 22:33:02 -0700

    add sidenav

--- src/components/shared/HamburgerMenuButton.tsx
+++ src/components/shared/HamburgerMenuButton.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 
+import SideNav from "../sidenav/SideNav";
+
 export const HamburgerMenuButton: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   return (
-    <div className="max-mobile-640:block hidden">
-      <button
-        className={`hamburger hamburger--slider ${
-          isMenuOpen ? "is-active" : ""
-        }`}
-        type="button"
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-      >
-        <span className="hamburger-box">
-          <span className="hamburger-inner"></span>
-        </span>
-      </button>
-    </div>
+    <>
+      <div className="max-mobile-640:block hidden">
+        <button
+          className={`hamburger hamburger--slider ${
+            isMenuOpen ? "is-active" : ""
+          }`}
+          type="button"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
+      </div>
+
+      <SideNav
+        isMenuOpen={isMenuOpen}
+        toggleMenu={() => setIsMenuOpen(!isMenuOpen)}
+      />
+    </>
   );
 };
 
